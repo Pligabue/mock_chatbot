b4b9a84cae6596dd0e53d086c444d72c14e87d0a
Pedro Ligabue <pliga@globo.com>
2020-03-30 17:33:29 -0300

    Create Mock Message List
    This mock message list only serves to occupy the remaining vertical
    space as of this commit.

---
+++ src/components/message-list/MessageList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import "./MessageList.scss"
+
+export default class MessageList extends Component {
+  render() {
+    return (
+      <div className="message-list"> Message List </div>
+    );
+  }
+}
