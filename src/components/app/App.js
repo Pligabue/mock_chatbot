2b63852c03169e5faa70162551200054c7689e56
Pedro Ligabue <pliga@globo.com>
2020-04-02 00:06:08 -0300

    Update App Styles

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,29 +7,29 @@ import Messages from '../messages/Messages';
 import Footer from '../footer/Footer';
 
 export default class App extends Component {
-  
+
   render() {        
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Switch>
-          <Route path="/login">
-            <h1>Login</h1>
-          </Route>
-          <Route path="/signup">
-            <h1>Sign Up</h1>
-          </Route>
-          <Route path="/messages">
-                <Messages />
-          </Route>
-          <Route path="/">
-            <h1>Home</h1>
-          </Route>
-        </Switch>
-      </div>
+    return (
+      <Router>
+        <div className="main-section">
+          <Header />
+            <Switch>
+            <Route path="/login">
+              <h1>Login</h1>
+            </Route>
+            <Route path="/signup">
+              <h1>Sign Up</h1>
+            </Route>
+            <Route path="/messages">
+              <Messages />
+            </Route>
+            <Route path="/">
+              <h1>Home</h1>
+            </Route>
+          </Switch>
+        </div>
         <Footer />
-    </Router>
-  );
-}
+      </Router>
+    );
+  }
 }
