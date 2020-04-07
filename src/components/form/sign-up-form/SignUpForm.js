ec6afda5979244875d60bc685e7c23e3dc8ab0cf
Pedro Ligabue <pliga@globo.com>
2020-04-07 12:20:32 -0300

    Create Sign Up Form

---
+++ src/components/form/sign-up-form/SignUpForm.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Form from '../Form';
+import { TextField, Button, Box } from '@material-ui/core';
+
+export default class SignUpForm extends Component {
+  render() {
+    return (
+      <Form title="Sign Up" action="Sign Up">
+        <Box display="flex" flexDirection="column">
+          <TextField 
+            name="email" 
+            type="email" 
+            label="E-mail" 
+            inputProps={{ style: { color: "black" } }} 
+            margin="normal" 
+            required 
+          />
+          <TextField 
+            name="password" 
+            type="password" 
+            label="Password" 
+            inputProps={{ style: { color: "black" } }} 
+            margin="normal" 
+            required 
+          />
+          <TextField 
+            name="password-confirmation" 
+            type="password" 
+            label="Confirm password" 
+            inputProps={{ style: { color: "black" } }} 
+            margin="normal" 
+            required 
+          />
+        </Box>
+      </Form>
+    );
+  }
+}
