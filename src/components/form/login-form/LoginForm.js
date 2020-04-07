f2160e189a5552fe7d73a1bd562174e903dd32a9
Pedro Ligabue <pliga@globo.com>
2020-04-07 12:20:17 -0300

    Create Login Form

---
+++ src/components/form/login-form/LoginForm.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Form from '../Form';
+import { TextField, Button, Box } from '@material-ui/core';
+
+export default class LoginForm extends Component {
+  render() {
+    return (
+      <Form title="Log In" action="Log In">
+        <Box display="flex" flexDirection="column">
+          <TextField 
+            name="email" 
+            type="email" 
+            label="E-mail" 
+            inputProps={{ style: { color: "black" } }} 
+            margin="normal" 
+            required 
+          />
+          <TextField 
+            name="password" 
+            type="password" 
+            label="Password" 
+            inputProps={{ style: { color: "black" } }} 
+            margin="normal" 
+            required 
+          />
+        </Box>
+      </Form>
+    );
+  }
+}
