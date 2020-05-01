bde015269685c53561cd3aae096205bf90222cfe
Pedro Ligabue <pliga@globo.com>
2020-05-01 20:14:58 -0300

    Add auto generated date messages

--- src/components/messages/Messages.js
+++ src/components/messages/Messages.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react';
 import MessageList from "./components/message-list/MessageList"
 import MessageInput from "./components/message-input/MessageInput"
 import Box from '@material-ui/core/Box';
+import { sameDay, formatTime } from '../../utils';
 
 export default class Messages extends Component {
+
+  render() {
+    return (
+      <Box height="100%" width="100%" display="flex" flexDirection="column" boxSizing="border-box" p={3}> 
+        <MessageList messages={this.state.messages} />
+        <MessageInput handleChange={this.handleMessageInput} handleSubmit={this.handleMessageSubmit} enabled={this.state.connected} />
+      </Box>
+    );
+  }
 
   constructor(props) {
     super(props);
@@ -32,34 +42,25 @@ export default class Messages extends Component {
       console.log("DISCONNECTED") 
     }
     this.websocket.onmessage = (e) => {
-      let data = JSON.parse(e.data)
-      this.addMessage(data.content, data.origin, data.date)
+      let { content, origin, date } = JSON.parse(e.data)
+      this.addNewMessage(content, origin, date)
+      this.addDateMessages()
     }
+    this.addDateMessages()
   }
 
   handleMessageInput = (e) => {
-    this.setState({
-      newMessageContent: e.target.value
-    })
+    this.setState({ newMessageContent: e.target.value })
   }
 
   handleMessageSubmit = (e) => {
     e.preventDefault();
-    e.target.reset();
-
-    if (this.state.newMessageContent) {
+    
+    if (!/^[ ]*$/.test(this.state.newMessageContent)) {
+      e.target.reset();
       this.sendMessage()
       this.cleanInput()
     }
-  }
-
-  render() {
-    return (
-      <Box height="100%" width="100%" display="flex" flexDirection="column" boxSizing="border-box" p={3}> 
-        <MessageList messages={this.state.messages} />
-        <MessageInput handleMessageInput={this.handleMessageInput} handleMessageSubmit={this.handleMessageSubmit} enabled={this.state.connected} />
-      </Box>
-    );
   }
 
   sendMessage = () => {
@@ -68,10 +69,18 @@ export default class Messages extends Component {
       origin: "user",
       date: new Date().toISOString()
     }))
-    this.addMessage(this.state.newMessageContent, "user", new Date().toISOString())
+    this.addNewMessage(this.state.newMessageContent, "user", new Date().toISOString())
   }
 
-  addMessage = (content, origin, date) => {
+  cleanInput = () => {
+    this.setState({
+      newMessageContent: "",
+      newMessageOrigin: "user",
+      newMessageDate: null,
+    })
+  }
+
+  addNewMessage = (content, origin, date) => {
     this.setState({
       messages: [...this.state.messages, {
         content: content,
@@ -81,11 +90,32 @@ export default class Messages extends Component {
     })
   }
 
-  cleanInput = () => {
-    this.setState({
-      newMessageContent: "",
-      newMessageOrigin: "user",
-      newMessageDate: null,
-    })
+  addDateMessages = () => {
+    this.sortMessages()
+    let { messages } = this.state
+    let datedMessages = []
+    let datedIndex = 0
+    let lastDate = messages[0].date
+    for (let i = 0; i < messages.length; i++) {
+      let message = messages[i]
+      if ((!sameDay(new Date(message.date), new Date(lastDate)) || i === 0) && message.origin !== "date") {
+        datedMessages[datedIndex] = { origin: "date", content: formatTime.day(new Date(message.date)), date: message.date }
+        datedMessages[datedIndex+1] = message
+        datedIndex += 2
+      } else {
+        datedMessages[datedIndex] = message
+        datedIndex += 1
+      }
+      lastDate = message.date
+    }
+    this.setState({ messages: datedMessages })
+  }
+
+  sortMessages = () => {
+    let { messages } = this.state
+    messages.sort((a, b) => (
+      new Date(a.date) < new Date(b.date) ? -1 : 1
+    ))
+    this.setState({ messages })
   }
 }
--- src/components/messages/components/message-list/MessageList.js
+++ src/components/messages/components/message-list/MessageList.js
@@ -4,6 +4,7 @@ import "./MessageList.scss"
 
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+import { formatTime } from '../../../../utils';
 
 export default class MessageList extends Component {
   
@@ -43,27 +44,27 @@ function Message(props) {
   let { origin, date } = props;
   if (origin === undefined) {
     throw new Error("Missing 'origin' parameter.")
-  } else if (origin !== "bot" && origin !== "user") {
-    throw new Error("Origin can only be 'user' or 'bot'.")
+  } else if (origin !== "bot" && origin !== "user" && origin !== "date") {
+    throw new Error("Origin can only be 'user', 'bot' or 'date'.")
   }
   let content = props.children
    
   return (
     <Box 
-      textAlign={origin === "user" ? "right" : "left"}
+      textAlign={origin === "date" ? "center" : (origin === "user" ? "right" : "left")}
       pt={2}
     >
       <Box
         display="inline-block"
         position="relative"
         maxWidth={0.7}
-        bgcolor={origin === "user" ? "primary.light" : "primary.dark"}
+        bgcolor={origin === "date" ? "secondary.light" : (origin === "user" ? "primary.light" : "primary.dark")}
         p={2}
         borderRadius={4}
         overflow="hidden"
       >
-        <Typography color="textPrimary" component="p" style={{ wordBreak: "break-word" }}>{content}</Typography>
-        <span style={{ color: "white", position: "absolute", right: "0", bottom: "0", fontSize: "0.6rem", padding: "0.2rem" }}>{date}</span>
+        <Typography color={origin === "date" ? "textSecondary" : "textPrimary"} component="p" style={{ wordBreak: "break-word" }}>{content}</Typography>
+        {origin !== "date" && <span style={{ color: "white", position: "absolute", right: "0", bottom: "0", fontSize: "0.6rem", padding: "0.2rem" }}>{formatTime.hour(new Date(date))}</span>}
       </Box>
     </Box>
   )
